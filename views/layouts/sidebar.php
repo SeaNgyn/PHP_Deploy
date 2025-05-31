b7afe9b763757462717bec69cbd13d0803ba9095
SeaNgyn <nguyentheanh2k3@gmail.com>
2025-05-31 15:24:07 +0700

    delete to create new ones

--- views/layouts/sidebar.php
+++
@@ -1,8 +0,0 @@
-<ul style="height: 100vh;">
-    <li style="margin-top: 27px;"><a href="home.php">Trang chủ</a></li>
-    <li><a href="profile.php">Hồ sơ</a></li>
-    <li><a href="search.php">Tìm kiếm</a></li>
-    <li><a href="dashboard.php">Giảng dạy</a></li>
-    <li><a href="manager.php">Quản Lý</a></li>
-</ul>
-
